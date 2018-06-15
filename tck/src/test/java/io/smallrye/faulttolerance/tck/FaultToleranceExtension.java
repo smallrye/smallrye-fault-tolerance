78d46aeeffa21ea8047b9c53f5644bdd02819dce
Antoine Sabot-Durand <antoine@sabot-durand.net>
2018-06-15 12:03:49 +0200

    Correct arquillian extension
    Signed-off-by: Antoine Sabot-Durand <antoine@sabot-durand.net>

--- tck/src/test/java/io/smallrye/faulttolerance/tck/FaultToleranceExtension.java
+++ tck/src/test/java/io/smallrye/faulttolerance/tck/FaultToleranceExtension.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2017 Red Hat, Inc.
+ * Copyright 2017 Red Hat, Inc, and individual contributors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,18 +13,20 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package io.smallrye.faulttolerance.tck;
 
-package io.smallrye.config;
-
-import org.jboss.arquillian.container.test.spi.client.deployment.AuxiliaryArchiveAppender;
+import org.jboss.arquillian.container.test.spi.client.deployment.ApplicationArchiveProcessor;
 import org.jboss.arquillian.core.spi.LoadableExtension;
 
 /**
- * @author <a href="http://jmesnil.net/">Jeff Mesnil</a> (c) 2017 Red Hat inc.
+ *
+ * @author Martin Kouba
  */
-public class SmallRyeFaultToleranceExtension implements LoadableExtension {
+public class FaultToleranceExtension implements LoadableExtension {
+
     @Override
-    public void register(ExtensionBuilder extensionBuilder) {
-        extensionBuilder.service(AuxiliaryArchiveAppender.class, SmallRyeFaultToleranceAuxiliaryArchiveAppender.class);
+    public void register(ExtensionBuilder builder) {
+        builder.service(ApplicationArchiveProcessor.class, FaultToleranceApplicationArchiveProcessor.class);
     }
+
 }
