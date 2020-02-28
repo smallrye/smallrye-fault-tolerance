fe39669804af451b6e798f39467e902b0b96d0ed
Ladislav Thon <lthon@redhat.com>
2020-02-28 10:56:10 +0100

    clarify API stability note in the "core" module

--- implementation/core/src/main/java/io/smallrye/faulttolerance/core/package-info.java
+++ implementation/core/src/main/java/io/smallrye/faulttolerance/core/package-info.java
@@ -3,6 +3,6 @@
  * The strategies here are not meant to be used directly.
  * Their API is not optimized for end user friendliness, but for integration correctness.
  * The core abstraction is {@link io.smallrye.faulttolerance.core.FaultToleranceStrategy}; each strategy implements that.
- * API stability is <b>not</b> guaranteed!
+ * API stability in this package and all its subpackages is <b>not</b> guaranteed!
  */
 package io.smallrye.faulttolerance.core;
