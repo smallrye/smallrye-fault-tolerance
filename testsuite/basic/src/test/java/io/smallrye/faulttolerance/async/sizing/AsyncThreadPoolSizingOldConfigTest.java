e8c2c008fea887c2b615d9d94a1e161f872021a1
Ladislav Thon <lthon@redhat.com>
2020-08-05 14:31:37 +0200

    rename the "globalThreadPoolSize" config property to "mainThreadPoolSize"
    The old config property is still used as a fallback, so this change
    is backwards compatible.

---
+++ testsuite/basic/src/test/java/io/smallrye/faulttolerance/async/sizing/AsyncThreadPoolSizingOldConfigTest.java
@@ -0,0 +1,21 @@
+package io.smallrye.faulttolerance.async.sizing;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.runner.RunWith;
+
+import io.smallrye.faulttolerance.TestArchive;
+
+@RunWith(Arquillian.class)
+public class AsyncThreadPoolSizingOldConfigTest extends AbstractAsyncThreadPoolSizingTest {
+    private static final String CONFIG = "io.smallrye.faulttolerance.globalThreadPoolSize=" + SIZE;
+
+    @Deployment
+    public static JavaArchive createTestArchive() {
+        return TestArchive.createBase(AsyncThreadPoolSizingOldConfigTest.class)
+                .addPackage(AsyncThreadPoolSizingOldConfigTest.class.getPackage())
+                .addAsManifestResource(new StringAsset(CONFIG), "microprofile-config.properties");
+    }
+}
