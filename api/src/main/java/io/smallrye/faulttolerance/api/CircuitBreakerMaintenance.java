e44975eccd148d75ca9340ecd5d4e4fa5e8f446f
Ladislav Thon <lthon@redhat.com>
2020-10-12 13:20:31 +0200

    rework the circuit breaker maintenance API
    Review and discussion of the circuit breaker maintenance API showed
    that the original API based on injecting `CircuitBreakerMaintenance`
    for named circuit breakers doesn't satisfy at least one use case:
    isolating tests. Between tests, it is required to reset all existing
    circuit breakers.
    
    The new API is a bit simpler and satisfies this use case nicely.
    There's just one `CircuitBreakerMaintenance` object for all circuit
    breakers. It allows reading and resetting state of a single circuit
    breaker, identified by `@CircuitBreakerName`, and resetting all
    circuit breakers at once.
    
    It is now checked at runtime whether given name was actually assigned
    to some circuit breaker. This is externalized to a special bean,
    because it needs to be done differently in Quarkus.

--- api/src/main/java/io/smallrye/faulttolerance/api/CircuitBreakerMaintenance.java
+++ api/src/main/java/io/smallrye/faulttolerance/api/CircuitBreakerMaintenance.java
@@ -18,26 +18,35 @@ package io.smallrye.faulttolerance.api;
 import io.smallrye.common.annotation.Experimental;
 
 /**
- * Allows reading current circuit breaker state and reseting it to initial (closed) state.
- * The circuit breaker must be given a name using {@link CircuitBreakerName @CircuitBreakerName},
- * and then {@code CircuitBreakerMaintenance} can be {@code @Inject}-ed.
- * <p>
- * It is an error if {@code CircuitBreakerMaintenance} is injected
- * without using the {@code @CircuitBreakerName} qualifier.
+ * Allows reading current state of circuit breakers and reseting them to the initial (closed) state.
+ * To access a specific circuit breaker, it must be given a name using {@link CircuitBreakerName @CircuitBreakerName}.
  */
 @Experimental("first attempt at providing maintenance access to circuit breakers")
 public interface CircuitBreakerMaintenance {
     /**
-     * Returns current state of the circuit breaker.
+     * Returns current state of the circuit breaker with given {@code name}.
      * Note that there's no guarantee that the circuit breaker will stay in that state for any time,
      * so this method is only useful for monitoring.
+     * <p>
+     * It is an error to use a {@code name} that wasn't registered using {@code @CircuitBreakerName}.
      */
-    CircuitBreakerState currentState();
+    CircuitBreakerState currentState(String name);
 
     /**
-     * Resets the circuit breaker to the initial (that is, closed) state.
+     * Resets the circuit breaker with given {@code name} to the initial (closed) state.
+     * <p>
+     * This method should not be used regularly, it's mainly meant for testing (to isolate individual tests)
+     * and perhaps emergency maintenance tasks.
+     * <p>
+     * It is an error to use a {@code name} that wasn't registered using {@code @CircuitBreakerName}.
+     */
+    void reset(String name);
+
+    /**
+     * Resets all circuit breakers (including those without a name) in the application to the initial (closed) state.
+     * <p>
      * This method should not be used regularly, it's mainly meant for testing (to isolate individual tests)
      * and perhaps emergency maintenance tasks.
      */
-    void reset();
+    void resetAll();
 }
---
+++ implementation/fault-tolerance/src/main/java/io/smallrye/faulttolerance/CircuitBreakerMaintenanceImpl.java
@@ -0,0 +1,71 @@
+package io.smallrye.faulttolerance;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import io.smallrye.faulttolerance.api.CircuitBreakerMaintenance;
+import io.smallrye.faulttolerance.api.CircuitBreakerState;
+import io.smallrye.faulttolerance.core.circuit.breaker.CircuitBreaker;
+
+@Singleton
+public class CircuitBreakerMaintenanceImpl implements CircuitBreakerMaintenance {
+    private final ConcurrentMap<String, CircuitBreaker<?>> registry = new ConcurrentHashMap<>();
+
+    private final ExistingCircuitBreakerNames existingCircuitBreakerNames;
+
+    @Inject
+    public CircuitBreakerMaintenanceImpl(ExistingCircuitBreakerNames existingCircuitBreakerNames) {
+        this.existingCircuitBreakerNames = existingCircuitBreakerNames;
+    }
+
+    void register(String circuitBreakerName, CircuitBreaker<?> circuitBreaker) {
+        registry.putIfAbsent(circuitBreakerName, circuitBreaker);
+    }
+
+    @Override
+    public CircuitBreakerState currentState(String name) {
+        if (!existingCircuitBreakerNames.contains(name)) {
+            throw new IllegalArgumentException("Circuit breaker '" + name + "' doesn't exist");
+        }
+
+        CircuitBreaker<?> circuitBreaker = registry.get(name);
+        if (circuitBreaker == null) {
+            // if the circuit breaker wasn't instantiated yet, it's "closed" by definition
+            return CircuitBreakerState.CLOSED;
+        }
+
+        int currentState = circuitBreaker.currentState();
+        switch (currentState) {
+            case CircuitBreaker.STATE_CLOSED:
+                return CircuitBreakerState.CLOSED;
+            case CircuitBreaker.STATE_OPEN:
+                return CircuitBreakerState.OPEN;
+            case CircuitBreaker.STATE_HALF_OPEN:
+                return CircuitBreakerState.HALF_OPEN;
+            default:
+                throw new IllegalStateException("Unknown circuit breaker state " + currentState);
+        }
+    }
+
+    @Override
+    public void reset(String name) {
+        if (!existingCircuitBreakerNames.contains(name)) {
+            throw new IllegalArgumentException("Circuit breaker '" + name + "' doesn't exist");
+        }
+
+        CircuitBreaker<?> circuitBreaker = registry.get(name);
+        if (circuitBreaker != null) {
+            // if the circuit breaker wasn't instantiated yet, "resetting it" is by definition a noop
+            circuitBreaker.reset();
+        }
+    }
+
+    @Override
+    public void resetAll() {
+        // circuit breakers that weren't instantiated yet don't have to be reset
+        registry.values().forEach(CircuitBreaker::reset);
+    }
+}
