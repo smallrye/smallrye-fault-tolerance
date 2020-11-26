ac5419ffe0f248154e39d96ccd3c81a38a82fdf0
Ladislav Thon <lthon@redhat.com>
2020-11-26 12:30:23 +0100

    improve circuit breaker to reject excess attempts in half-open
    Previously, circuit breakers in the half-open state would let
    all invocations go through. In case of any failure, it would
    move back to open, and in case of a configured number of consecutive
    successes, it would move to closed.
    
    This is fine and passes the TCK, but under high load, this can
    cause a thundering herd problem on the guarded service.
    
    A better solution, implemented by this commit, is: in half-open,
    only allow the first `successThreshold` invocations to go through
    (also known as "probe invocations"), and reject the rest. If any
    of those probe invocations fail, move back to open; if all of them
    succeed, move to closed.

---
+++ testsuite/basic/src/test/java/io/smallrye/faulttolerance/circuitbreaker/halfopen/HelloService.java
@@ -0,0 +1,37 @@
+package io.smallrye.faulttolerance.circuitbreaker.halfopen;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
+
+import io.smallrye.faulttolerance.core.util.party.Party;
+
+@ApplicationScoped
+public class HelloService {
+    static final int ROLLING_WINDOW = 10;
+    static final int DELAY = 200;
+    static final int PROBE_ATTEMPTS = 5;
+
+    private final AtomicInteger counter = new AtomicInteger(0);
+
+    @CircuitBreaker(requestVolumeThreshold = ROLLING_WINDOW, delay = DELAY, successThreshold = PROBE_ATTEMPTS)
+    public String hello(boolean fail, Party.Participant participant) throws InterruptedException {
+        counter.incrementAndGet();
+
+        if (fail) {
+            throw new IllegalArgumentException();
+        }
+
+        if (participant != null) {
+            participant.attend();
+        }
+
+        return "Hello, world!";
+    }
+
+    AtomicInteger getCounter() {
+        return counter;
+    }
+}
