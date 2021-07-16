b24ce9ec9b74d1bafd3cbd29a27919c44709189f
Ladislav Thon <lthon@redhat.com>
2021-07-16 15:08:07 +0200

    remove validation of method return types for @Blocking and @NonBlocking

--- implementation/fault-tolerance/src/main/java/io/smallrye/faulttolerance/config/BlockingConfig.java
+++ implementation/fault-tolerance/src/main/java/io/smallrye/faulttolerance/config/BlockingConfig.java
@@ -1,9 +1,6 @@
 package io.smallrye.faulttolerance.config;
 
-import org.eclipse.microprofile.faulttolerance.exceptions.FaultToleranceDefinitionException;
-
 import io.smallrye.common.annotation.Blocking;
-import io.smallrye.faulttolerance.AsyncTypes;
 import io.smallrye.faulttolerance.autoconfig.AutoConfig;
 import io.smallrye.faulttolerance.autoconfig.Config;
 
@@ -11,9 +8,7 @@ import io.smallrye.faulttolerance.autoconfig.Config;
 public interface BlockingConfig extends Blocking, Config {
     @Override
     default void validate() {
-        if (!AsyncTypes.isKnown(method().returnType)) {
-            throw new FaultToleranceDefinitionException("Invalid @Blocking on " + method()
-                    + ": must return " + AsyncValidation.describeKnownAsyncTypes());
-        }
+        // we don't validate the method return type, because the @Blocking annotation
+        // may be present for another framework and we're supposed to ignore it
     }
 }
--- implementation/fault-tolerance/src/main/java/io/smallrye/faulttolerance/config/NonBlockingConfig.java
+++ implementation/fault-tolerance/src/main/java/io/smallrye/faulttolerance/config/NonBlockingConfig.java
@@ -1,9 +1,6 @@
 package io.smallrye.faulttolerance.config;
 
-import org.eclipse.microprofile.faulttolerance.exceptions.FaultToleranceDefinitionException;
-
 import io.smallrye.common.annotation.NonBlocking;
-import io.smallrye.faulttolerance.AsyncTypes;
 import io.smallrye.faulttolerance.autoconfig.AutoConfig;
 import io.smallrye.faulttolerance.autoconfig.Config;
 
@@ -11,9 +8,7 @@ import io.smallrye.faulttolerance.autoconfig.Config;
 public interface NonBlockingConfig extends NonBlocking, Config {
     @Override
     default void validate() {
-        if (!AsyncTypes.isKnown(method().returnType)) {
-            throw new FaultToleranceDefinitionException("Invalid @NonBlocking on " + method()
-                    + ": must return " + AsyncValidation.describeKnownAsyncTypes());
-        }
+        // we don't validate the method return type, because the @NonBlocking annotation
+        // may be present for another framework and we're supposed to ignore it
     }
 }
